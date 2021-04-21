045d041ab5fddf689baa4e4e0792ca887c050098
Nima Shoghi <nimashoghi@gmail.com>
2021-04-21 16:39:44 +0000

    Update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const fs = require('fs');
 const path = require('path');
 const program = require('commander');
-const { Source, buildSchema } = require('graphql');
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+const { buildASTSchema } = require('graphql');
 const del = require('del');
 
 program
@@ -13,18 +15,19 @@ program
   .parse(process.argv);
 
 const { schemaFilePath, destDirPath, depthLimit = 100, includeDeprecatedFields = false } = program;
-const typeDef = fs.readFileSync(schemaFilePath, "utf-8");
-const source = new Source(typeDef);
-const gqlSchema = buildSchema(source);
+const gqlSchema = buildASTSchema(mergeTypeDefs(loadFilesSync(schemaFilePath)));
 
 del.sync(destDirPath);
-path.resolve(destDirPath).split(path.sep).reduce((before, cur) => {
-  const pathTmp = path.join(before, cur + path.sep);
-  if (!fs.existsSync(pathTmp)) {
-    fs.mkdirSync(pathTmp);
-  }
-  return path.join(before, cur + path.sep);
-}, '');
+path
+  .resolve(destDirPath)
+  .split(path.sep)
+  .reduce((before, cur) => {
+    const pathTmp = path.join(before, cur + path.sep);
+    if (!fs.existsSync(pathTmp)) {
+      fs.mkdirSync(pathTmp);
+    }
+    return path.join(before, cur + path.sep);
+  }, '');
 let indexJsExportAll = '';
 
 /**
@@ -33,39 +36,38 @@ let indexJsExportAll = '';
  * @param duplicateArgCounts map for deduping argument name collisions
  * @param allArgsDict dictionary of all arguments
  */
-const getFieldArgsDict = (
-  field,
-  duplicateArgCounts,
-  allArgsDict = {},
-) => field.args.reduce((o, arg) => {
-  if (arg.name in duplicateArgCounts) {
-    const index = duplicateArgCounts[arg.name] + 1;
-    duplicateArgCounts[arg.name] = index;
-    o[`${arg.name}${index}`] = arg;
-  } else if (allArgsDict[arg.name]) {
-    duplicateArgCounts[arg.name] = 1;
-    o[`${arg.name}1`] = arg;
-  } else {
-    o[arg.name] = arg;
-  }
-  return o;
-}, {});
+const getFieldArgsDict = (field, duplicateArgCounts, allArgsDict = {}) =>
+  field.args.reduce((o, arg) => {
+    if (arg.name in duplicateArgCounts) {
+      const index = duplicateArgCounts[arg.name] + 1;
+      duplicateArgCounts[arg.name] = index;
+      o[`${arg.name}${index}`] = arg;
+    } else if (allArgsDict[arg.name]) {
+      duplicateArgCounts[arg.name] = 1;
+      o[`${arg.name}1`] = arg;
+    } else {
+      o[arg.name] = arg;
+    }
+    return o;
+  }, {});
 
 /**
  * Generate variables string
  * @param dict dictionary of arguments
  */
-const getArgsToVarsStr = dict => Object.entries(dict)
-  .map(([varName, arg]) => `${arg.name}: $${varName}`)
-  .join(', ');
+const getArgsToVarsStr = (dict) =>
+  Object.entries(dict)
+    .map(([varName, arg]) => `${arg.name}: $${varName}`)
+    .join(', ');
 
 /**
  * Generate types string
  * @param dict dictionary of arguments
  */
-const getVarsToTypesStr = dict => Object.entries(dict)
-  .map(([varName, arg]) => `$${varName}: ${arg.type}`)
-  .join(', ');
+const getVarsToTypesStr = (dict) =>
+  Object.entries(dict)
+    .map(([varName, arg]) => `$${varName}: ${arg.type}`)
+    .join(', ');
 
 /**
  * Generate the query for the specified field
@@ -84,7 +86,7 @@ const generateQuery = (
   argumentsDict = {},
   duplicateArgCounts = {},
   crossReferenceKeyList = [], // [`${curParentName}To${curName}Key`]
-  curDepth = 1,
+  curDepth = 1
 ) => {
   const field = gqlSchema.getType(curParentType).getFields()[curName];
   const curTypeName = field.type.inspect().replace(/[[\]!]/g, '');
@@ -98,14 +100,17 @@ const generateQuery = (
     crossReferenceKeyList.push(crossReferenceKey);
     const childKeys = Object.keys(curType.getFields());
     childQuery = childKeys
-      .filter(fieldName => {
+      .filter((fieldName) => {
         /* Exclude deprecated fields */
         const fieldSchema = gqlSchema.getType(curType).getFields()[fieldName];
         return includeDeprecatedFields || !fieldSchema.isDeprecated;
       })
-      .map(cur => generateQuery(cur, curType, curName, argumentsDict, duplicateArgCounts,
-        crossReferenceKeyList, curDepth + 1).queryStr)
-      .filter(cur => cur)
+      .map(
+        (cur) =>
+          generateQuery(cur, curType, curName, argumentsDict, duplicateArgCounts, crossReferenceKeyList, curDepth + 1)
+            .queryStr
+      )
+      .filter((cur) => cur)
       .join('\n');
   }
 
@@ -133,9 +138,19 @@ const generateQuery = (
         const valueTypeName = types[i];
         const valueType = gqlSchema.getType(valueTypeName);
         const unionChildQuery = Object.keys(valueType.getFields())
-          .map(cur => generateQuery(cur, valueType, curName, argumentsDict, duplicateArgCounts,
-            crossReferenceKeyList, curDepth + 2).queryStr)
-          .filter(cur => cur)
+          .map(
+            (cur) =>
+              generateQuery(
+                cur,
+                valueType,
+                curName,
+                argumentsDict,
+                duplicateArgCounts,
+                crossReferenceKeyList,
+                curDepth + 2
+              ).queryStr
+          )
+          .filter((cur) => cur)
           .join('\n');
         queryStr += `${fragIndent}... on ${valueTypeName} {\n${unionChildQuery}\n${fragIndent}}\n`;
       }
@@ -151,7 +166,7 @@ const generateQuery = (
  * @param description description of the current object
  */
 const generateFile = (obj, description) => {
-  let indexJs = 'const fs = require(\'fs\');\nconst path = require(\'path\');\n\n';
+  let indexJs = "const fs = require('fs');\nconst path = require('path');\n\n";
   let outputFolderName;
   switch (description) {
     case 'Mutation':
@@ -170,7 +185,7 @@ const generateFile = (obj, description) => {
   try {
     fs.mkdirSync(writeFolder);
   } catch (err) {
-    if (err.code !== 'EEXIST') throw err
+    if (err.code !== 'EEXIST') throw err;
   }
   Object.keys(obj).forEach((type) => {
     const field = gqlSchema.getType(description).getFields()[type];
